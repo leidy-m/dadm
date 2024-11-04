ed188267b5442718ecc7955138b0c9f1154501e8
leidy-m <7032681@gmail.com>
2024-11-04 18:30:38 -0500

    Hola Mundo en Android

---
+++ reto-0/Readme.md
@@ -0,0 +1,26 @@
+# Reto 0
+
+Muestra el texto "Hola Mundo!" en Android, Utiliza Surface para crear un fondo color Magenta y padding.
+Cambie el nombre de la aplicación en el archivo: app/src/main/res/values/strings.xml 
+
+Documentación: https://developer.android.com/codelabs/basic-android-kotlin-compose-first-app?hl=es-419#5
+
+
+Habilitar Android (real device):
+
+* Acerca del teléfono -> Versión MIUI 
+
+* Settings -> Additional Settings -> Developer options ->
+
+* Turn off "MIUI optimization" and Restart
+
+* Turn On "USB Debugging"
+
+* Turn On "Install via USB"
+
+* MTP(Media Transfer Protocol) is the default mode.
+    Works even in MTP in some cases
+
+* Set USB Configuration to Charging
+
+
---
+++ reto-0/app/src/main/java/com/example/helloworld/MainActivity.kt
@@ -0,0 +1,53 @@
+package com.example.helloworld
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.helloworld.ui.theme.HelloWorldTheme
+import androidx.compose.ui.unit.dp
+
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            HelloWorldTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Mundo *.*",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Surface(color = Color.Magenta) {
+        Text(
+            text = "*w* Hola $name !",
+            modifier = Modifier.padding(64.dp)
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    HelloWorldTheme {
+        Greeting("Mundo")
+    }
+}
